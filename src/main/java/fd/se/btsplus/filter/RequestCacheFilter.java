895f4498a5606875d556cd87defbe866339e4816
VigilGLC <18302010038@fudan.edu.cn>
2021-04-10 22:01:13 +0800

    Add Cache Filter.

---
+++ src/main/java/fd/se/btsplus/filter/RequestCacheFilter.java
@@ -0,0 +1,81 @@
+package fd.se.btsplus.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ReadListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@AllArgsConstructor
+@Component
+public class RequestCacheFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        request = new CachedBodyHttpServletRequest(request);
+        filterChain.doFilter(request, response);
+    }
+
+    private static class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
+
+        private final byte[] cachedBody;
+
+        public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
+            super(request);
+            InputStream requestInputStream = request.getInputStream();
+            this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+        }
+
+        @Override
+        public ServletInputStream getInputStream() {
+            return new CachedBodyServletInputStream(this.cachedBody);
+        }
+
+        @Override
+        public BufferedReader getReader() {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+            return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+        }
+
+
+    }
+
+    private static class CachedBodyServletInputStream extends ServletInputStream {
+
+        private final InputStream cachedBodyInputStream;
+
+        public CachedBodyServletInputStream(byte[] cachedBody) {
+            this.cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
+        }
+
+        @Override
+        public int read() throws IOException {
+            return cachedBodyInputStream.read();
+        }
+
+        @SneakyThrows
+        @Override
+        public boolean isFinished() {
+            return cachedBodyInputStream.available() == 0;
+        }
+
+        @Override
+        public boolean isReady() {
+            return true;
+        }
+
+        @Override
+        public void setReadListener(ReadListener listener) {
+        }
+    }
+}
