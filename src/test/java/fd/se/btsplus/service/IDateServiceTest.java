94aea244e77fa7bc4bcf94f86c42e46044cdb990
CappuccinoCup <CappuccinoCup@126.com>
2021-04-20 15:31:28 +0800

    IDateService test complete

--- src/test/java/fd/se/btsplus/service/IDateServiceTest.java
+++ src/test/java/fd/se/btsplus/service/IDateServiceTest.java
@@ -1,50 +1,134 @@
 package fd.se.btsplus.service;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import fd.se.btsplus.model.domain.DateEvent;
 import org.junit.jupiter.api.Test;
+
+import java.time.Period;
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class IDateServiceTest {
+    private IDateService iDateService;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    void testDayEqual() {
+        assertTrue(IDateService.dayEqual(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayEqual(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2021 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayEqual(
+                new Date(2001 - 1900, Calendar.JANUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayEqual(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 1),
+                new Date(2021 - 1900, Calendar.FEBRUARY, 3)
+        ));
     }
 
     @Test
-    void dayEqual() {
+    void testDayBefore() {
+        assertTrue(IDateService.dayBefore(
+                new Date(2000 - 1900, Calendar.JANUARY, 1),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayBefore(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayBefore(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 1)
+        ));
+        assertFalse(IDateService.dayBefore(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.JANUARY, 3)
+        ));
+        assertFalse(IDateService.dayBefore(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2000 - 1900, Calendar.FEBRUARY, 3)
+        ));
     }
 
     @Test
-    void dayBefore() {
+    void testDayAfter() {
+        assertTrue(IDateService.dayAfter(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2000 - 1900, Calendar.JANUARY, 1)
+        ));
+        assertFalse(IDateService.dayAfter(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayAfter(
+                new Date(2001 - 1900, Calendar.FEBRUARY, 1),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayAfter(
+                new Date(2001 - 1900, Calendar.JANUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
+        assertFalse(IDateService.dayAfter(
+                new Date(2000 - 1900, Calendar.FEBRUARY, 3),
+                new Date(2001 - 1900, Calendar.FEBRUARY, 3)
+        ));
     }
 
     @Test
-    void dayAfter() {
+    void testAddDate() {
+        Period period = Period.of(20, 2, 17);
+        Date base = new Date(2001 - 1900, Calendar.FEBRUARY, 3);
+
+        base = IDateService.addDate(base, period);
+        assertEquals(2021, base.getYear() + 1900);
+        assertEquals(Calendar.APRIL, base.getMonth());
+        assertEquals(20, base.getDate());
     }
 
     @Test
-    void addDate() {
+    void testDayAfter1() {
+        iDateService = new IDateServiceImpl();
+
+        Date date = iDateService.dateAfter(20, 2, 17, 11);
+        assertEquals(IDateServiceImpl.curr.getYear() + 20, date.getYear());
+        assertEquals(IDateServiceImpl.curr.getMonth() + 2, date.getMonth());
+        assertEquals(IDateServiceImpl.curr.getDate() + 17, date.getDate());
+        assertEquals(IDateServiceImpl.curr.getHours() + 11, date.getHours());
+
+        iDateService = null;
     }
 
     @Test
-    void currDate() {
+    void testDayAfter2() {
+        iDateService = new IDateServiceImpl();
+
+        Date date = iDateService.dateAfter(11);
+        assertEquals(IDateServiceImpl.curr.getYear(), date.getYear());
+        assertEquals(IDateServiceImpl.curr.getMonth(), date.getMonth());
+        assertEquals(IDateServiceImpl.curr.getDate(), date.getDate());
+        assertEquals(IDateServiceImpl.curr.getHours() + 11, date.getHours());
+
+        iDateService = null;
+    }
+}
+
+class IDateServiceImpl implements IDateService {
+    public final static Date curr = new Date(2001 - 1900, Calendar.FEBRUARY, 3);
+
+    @Override
+    public Date currDate() {
+        return curr;
     }
 
-    @Test
-    void emit() {
-    }
-
-    @Test
-    void dateAfter() {
-    }
-
-    @Test
-    void testDateAfter() {
+    @Override
+    public DateEvent emit() {
+        return null;
     }
 }
--- src/test/java/fd/se/btsplus/service/impl/SystemDateServiceTest.java
+++ src/test/java/fd/se/btsplus/service/impl/SystemDateServiceTest.java
@@ -1,44 +1,85 @@
 package fd.se.btsplus.service.impl;
 
-import org.apache.commons.lang3.time.DateUtils;
+import fd.se.btsplus.config.BtsProperties;
+import fd.se.btsplus.service.IDateService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationEventPublisher;
 
-import java.util.Calendar;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class SystemDateServiceTest {
+    private IDateService serviceWithoutDateStr;
+    private IDateService systemDateService;
 
-    @Test
-    void testDayOfTheMonth() {
-        final Date date = new Date();
-        final Date truncated = DateUtils.truncate(date, Calendar.DAY_OF_MONTH);
-        System.out.println(truncated);
-    }
+    private ApplicationEventPublisher publisher;
+    private BtsProperties btsProperties;
 
+    // launch-date for btsProperties
+    private final static String dateStr = "2021-04-20";
+
+    @SneakyThrows
     @BeforeEach
     void setUp() {
+        publisher = Mockito.mock(ApplicationContext.class);
+        btsProperties = Mockito.mock(BtsProperties.class);
+        Mockito.when(btsProperties.getLaunchDate()).thenReturn("").thenReturn(dateStr);
+
+        // test if branch in init()
+        serviceWithoutDateStr = new SystemDateService(publisher, btsProperties);
+        Method init = serviceWithoutDateStr.getClass().getDeclaredMethod("init");
+        init.invoke(serviceWithoutDateStr);
+
+        // create systemDateService for test case
+        systemDateService = new SystemDateService(publisher, btsProperties);
+        init = systemDateService.getClass().getDeclaredMethod("init");
+        init.invoke(systemDateService);
     }
 
     @AfterEach
     void tearDown() {
+        publisher = null;
+        btsProperties = null;
+        serviceWithoutDateStr = null;
+        systemDateService = null;
+    }
+
+    @SneakyThrows
+    @Test
+    void testCurrDate() {
+        Date currDate = new Date();
+        Date launchDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateStr);
+        LocalDate base = currDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate target = launchDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        Period offset = Period.between(base, target);
+
+        Date result = systemDateService.currDate();
+        assertEquals(currDate.getYear() + offset.getYears(), result.getYear());
+        assertEquals(currDate.getMonth() + offset.getMonths(), result.getMonth());
+        assertEquals(currDate.getDate() + offset.getDays(), result.getDate());
     }
 
     @Test
-    void init() {
+    void testEmit() {
+        // done in testDailyEmit()
     }
 
+    @SneakyThrows
     @Test
-    void currDate() {
-    }
-
-    @Test
-    void emit() {
-    }
-
-    @Test
-    void dailyEmit() {
+    void testDailyEmit() {
+        Method dailyEmit = systemDateService.getClass().getDeclaredMethod("dailyEmit");
+        dailyEmit.invoke(systemDateService);
     }
 }
